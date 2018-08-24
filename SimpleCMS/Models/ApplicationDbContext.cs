c51fc954c7fb4c0ce4c42d68b220d3937a11dc58
xuechi <113439453@qq.com>
2018-08-24 09:38:44 +0800

    增加model

---
+++ SimpleCMS/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNet.Identity.EntityFramework;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCMS.Models
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser, ApplicationRole, int, ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+     
+}
